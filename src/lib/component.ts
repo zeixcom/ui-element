c008f0583cdb21e1251a7baab8a818851a284cf8
Esther Brunner <esther.brunner@zeix.com>
2025-03-24 06:37:17 +0100

    [WIP] feat: component

--- src/lib/component.ts
+++ src/lib/component.ts
@@ -1,46 +1,109 @@
+import { state, toSignal, type ComputedCallbacks } from "@zeix/cause-effect"
+
 import { isFunction } from "../core/util"
-import { UIElement, type ComponentSignals, type SignalInitializer } from "../ui-element"
 
 /* === Types === */
 
+export type ComponentSignals = { [key: string]: {} }
+
+export type Component<S extends ComponentSignals> = HTMLElement & S
+
+export type AttributeParser<T, S extends ComponentSignals> = (
+	host: Component<S>,
+	value: string | null,
+	old?: string | null
+) => T
+
+export type Root<S extends ComponentSignals> = ShadowRoot | Component<S>
+
+export type SignalInitializer<T, S extends ComponentSignals> = T
+	| AttributeParser<T, S>
+	| ComputedCallbacks<NonNullable<T>, []>
+
+export type FxFunction<S extends ComponentSignals> = (
+	host: Component<S>,
+	target?: Element,
+	index?: number
+) => ((() => void) | void)[]
+
 export type ComponentSetup<S extends ComponentSignals> = (
-	host: UIElement<S>,
+	host: Component<S>,
 	signals: S
-) => void | (() => void)
+) => FxFunction<S>[]
 
 /* === Internal Functions === */
 
-const camelToKebab = /*#__PURE__*/ (str: string): string =>
-	str.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase()
+const runFx = <S extends ComponentSignals>(
+	fns: FxFunction<S>[],
+	...args: [Component<S>, Element?, number?]
+) => fns.flatMap(fn => isFunction(fn) ? [fn(...args)].filter(isFunction) : [])
+
+const isAttributeParser = <T, S extends ComponentSignals>(value: unknown): value is AttributeParser<T, S> =>
+	isFunction(value) && value.length >= 2
 
 /* === Exported Functions === */
+
+export const first = <S extends ComponentSignals>(
+	selector: string,
+	...fns: FxFunction<S>[]
+) => (host: Component<S>) => {
+	const target = host.querySelector(selector)
+	return target ? runFx(fns, host, target) : []
+}
+
+export const all = <S extends ComponentSignals>(
+	selector: string,
+	...fns: FxFunction<S>[]
+) => (host: Component<S>) =>
+	Array.from(host.querySelectorAll(selector))
+		.flatMap((target, index) => runFx(fns, host, target, index))
 
 /**
  * Define a component with its states and setup function (connectedCallback)
  * 
- * @since 0.10.1
+ * @since 0.12.0
  * @param {string} name - name of the custom element
  * @param {{ [K in keyof S]: SignalInitializer<S[K], S> }} init - states of the component
- * @param {ComponentSetup<S>} setup - setup function to be called in connectedCallback(), may return cleanup function to be called in disconnectedCallback()
- * @returns {typeof Component} - the custom element class
+ * @param {FxFunction<S>[]} fx - setup function to be called in connectedCallback(), may return cleanup function to be called in disconnectedCallback()
+ * @returns {Component<S>} - the custom element class
  */
 export const component = <S extends ComponentSignals>(
 	name: string,
-	init: { [K in keyof S]: SignalInitializer<S[K], S> },
-	setup: ComponentSetup<S>
-  ): typeof UIElement => {
-	return (class extends UIElement {
-		static readonly localName = name
-		static get observedAttributes() {
-			return Object.keys(init).map(camelToKebab)
+	init: { [K in keyof S]: SignalInitializer<S[K], S>} = {} as { [K in keyof S]: SignalInitializer<S[K], S> },
+	fx: ComponentSetup<S>
+) => {
+	class Component<S> extends HTMLElement {
+		#signals: S = {} as S
+		#cleanup: (() => void)[] = []
+	  
+		static observedAttributes = Object.entries(init)
+			.filter(([, ini]) => isAttributeParser(ini))
+			.map(([prop]) => prop)
+	  
+		constructor() {
+			super()
+			for (const [prop, ini] of Object.entries(init)) {
+				const signal = isAttributeParser<{}, ComponentSignals>(ini)
+					? toSignal(ini(this, ''))
+					: state(ini)
+				Object.defineProperty(this, prop, signal)
+				this.#signals[prop] = signal
+			}
 		}
-
-		init = init
-
+	  
 		connectedCallback() {
-			super.connectedCallback()
-			const cleanup = setup(this as unknown as UIElement<S>, this.signals as S)
-			if (isFunction(cleanup)) this.cleanup.push(cleanup)
+			this.#cleanup.push(...runFx(fx(this, this.#signals), this))
 		}
-	}).define()
-}
+		
+		disconnectedCallback() {
+			for (const off of this.#cleanup) off()
+			this.#cleanup.length = 0
+		}
+		
+		attributeChangedCallback(prop: string, old: string | null, value: string | null) {
+			this[prop] = init[prop](this, value, old)
+		}
+	}
+	customElements.define(name, Component)
+	return Component
+  }
